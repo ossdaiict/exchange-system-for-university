c2c336b37eabab3a281e7f0094a849c99c732681
HarshGundecha <harsh.gundecha@gmail.com>
2019-04-08 06:45:05 +0530

    migration fix

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,8 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('session'=>'ss', 'form_validation'=>'fv', 'parser', 'migration');
+//$autoload['libraries'] = array('session'=>'ss', 'form_validation'=>'fv', 'parser', 'migration');
+$autoload['libraries'] = array('session'=>'ss', 'form_validation'=>'fv', 'parser');
 
 /*
 | -------------------------------------------------------------------
