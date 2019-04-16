40c182ba07ad97b734e85e19985efcefa501bd39
HarshGundecha <harsh.gundecha@gmail.com>
2019-04-16 22:00:45 +0530

    fixes

--- application/views/t_and_c.php
+++ application/views/t_and_c.php
@@ -18,6 +18,7 @@
 			<li class="list-group-item">You  should be the sole, absolute and lawful owner of the product sold by you. At any given time, if found, the product does not belong to you or is a stolen product, a strict punitive action shall be initiated against you.</li>
 			<li class="list-group-item">No Guarantee/Warranty: The institute/system shall not be responsible for the guarantee/warranty of the product being sold. The buyer has to satisfy himself/herself by the genuinity of the product, its functionality, operationality etc. This system does not make any Warranty as to specifics( such as quality, value , salability, etc) of the products proposed to be sold or purchased. It does not implicitly or explicitly support or endorse the sale  or purchase of any product on the website.</li>
 			<li class="list-group-item">The return policy for the bought productsis as per seller's choice, the system doesn't gurantee anything related to return.</li>
+			<li class="list-group-item">If a product is reported more than 10 times will be blocked from listing.</li>
 			<li class="list-group-item">Delivery/receipt of the sold product shall be the responsibility of the two parties hereinafter known as “Buyer & Seller”.</li>
 			<li class="list-group-item">Product Updation for sell: The product uploaded by the seller will be blocked from listing after three months (if unsold). It's seller’s responsibility to update the status of that product every three months.</li>
 		</ul>
